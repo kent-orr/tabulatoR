4bc94ea005fac9eaa5ff8a0f7593842613b5e59e
Kent Orr <42743597+kent-orr@users.noreply.github.com>
2025-08-13 10:39:50 -0400

    Add snapshots for ActionColumn tests

--- tests/testthat/test-columns.R
+++ tests/testthat/test-columns.R
@@ -10,23 +10,9 @@ test_that("Column returns expected list", {
 # Tests for ActionColumn
 
 test_that("ActionColumn returns expected list", {
-  js_code <- glue::glue(
-    "\n  function(cell, formatterParams, onRendered) {\n    const el = cell.getElement();\n    const Button = document.createElement('button');\n    Button.className = 'btn btn-primary';\n    Button.innerHTML = 'Edit';\n    Button.onclick = function() {\n      const table = cell.getTable();\n      const inputId = table.id;\n      const inputVal = Shiny.shinyapp.$inputValues[inputId] || {};\n      inputVal['edit'] = {\n        event: 'edit',\n        field: cell.getField(),\n        value: flattenData(cell.getValue()),\n        row: flattenData(cell.getRow().getData()),\n        position: flattenData(cell.getRow().getPosition())\n      };\n      Shiny.setInputValue(inputId, inputVal, { priority: 'event' });\n    };\n    el.appendChild(Button);\n  }\n  ", .open = "<<", .close = ">>")
-  expected <- Column(
-    title = "Edit",
-    field = "edit",
-    formatter = js(js_code)
-  )
-  expect_equal(ActionColumn("Edit", "edit"), expected)
+  expect_snapshot(ActionColumn("Edit", "edit"))
 })
 
 test_that("ActionColumn supports icons", {
-  js_code <- glue::glue(
-    "\n  function(cell, formatterParams, onRendered) {\n    const el = cell.getElement();\n    const Button = document.createElement('button');\n    Button.className = 'btn btn-primary';\n    Button.innerHTML = '<i class=\"fa fa-edit\" role=\"presentation\" aria-hidden=\"true\"></i>Edit';\n    Button.onclick = function() {\n      const table = cell.getTable();\n      const inputId = table.id;\n      const inputVal = Shiny.shinyapp.$inputValues[inputId] || {};\n      inputVal['edit'] = {\n        event: 'edit',\n        field: cell.getField(),\n        value: flattenData(cell.getValue()),\n        row: flattenData(cell.getRow().getData()),\n        position: flattenData(cell.getRow().getPosition())\n      };\n      Shiny.setInputValue(inputId, inputVal, { priority: 'event' });\n    };\n    el.appendChild(Button);\n  }\n  ", .open = "<<", .close = ">>")
-  expected <- Column(
-    title = "Edit",
-    field = "edit",
-    formatter = js(js_code)
-  )
-  expect_equal(ActionColumn("Edit", "edit", icon = shiny::icon("edit")), expected)
+  expect_snapshot(ActionColumn("Edit", "edit", icon = shiny::icon("edit")))
 })
